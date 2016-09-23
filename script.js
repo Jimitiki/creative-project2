faf334a4b3dde901acca084db89838aa6d01f731
Jimitiki <cwjohnson42@gmail.com>
2016-09-23 12:52:04 -0600

    Added initial files

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(initialize);
+
+function initialize() {
+    $("#search-btn").on("click", function() {
+        submitSearch();
+    });
+}
+
+function submitSearch() {
+    var url = "https://www.tastekid.com/api/similar?k=241156-MediaRec-C9L3QQKV&q=";
+    $(".media-row").each(function (index, el) {
+        url += $(".media-row .media-type").val() + ":";
+        var name = urlEncode($(".media-row .media-input").val());
+        url += name;
+        $.ajax({
+            dataType: "json",
+            url: url,
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    });
+}
+                         
+function urlEncode(str) {
+    str = str.replace(" ", "+");
+    str = str.toLowerCase();
+    return str;
+}
